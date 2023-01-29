471a373def2527ac0a0b4fbcd514d29c01fc785d
Art Schedrov <a.sch@tutanota.com>
2023-01-29 18:43:39 +0700

    fix generate potions

--- src/main/java/elements/Potion.java
+++ src/main/java/elements/Potion.java
@@ -1,21 +1,22 @@
 package elements;
 
 import util.ItemType;
-
 import java.util.Random;
 
 public class Potion extends Item {
     public static Potion potionOfHeal = new Potion(0, 0,"A Potion of Heal", "Yellow", ItemType.POTION);
     public static Potion potionOfCurse = new Potion(0, 0,"A Potion of Curse", "Black", ItemType.POTION);
     public static Potion potionOfDexterity = new Potion(0,0,"A Potion of Dexterity", "Green", ItemType.POTION);
+    public static Potion potionOfConstitution = new Potion(0,0,"A Potion of Constitution", "Brown", ItemType.POTION);
 
-    public static Potion[] potions = { potionOfHeal, potionOfCurse, potionOfDexterity };
+    public static Potion[] potions = { potionOfHeal, potionOfCurse, potionOfDexterity, potionOfConstitution};
 
     private static ItemType itemType;
 
     public Potion(int posX, int posY, String name, String color, ItemType itemType) {
         super(posX, posY, name, color, itemType);
         this.setPos(posX, posY);
+        System.out.println("[Potion]: Creating potion");
     }
 
     @Override
@@ -38,8 +39,13 @@ public class Potion extends Item {
     public void setPos(int x, int y) {
         super.setPos(x, y);
     }
+
+    private static Potion copyPotion(Potion potion) {
+        return new Potion(potion.posX, potion.posY, potion.getName(), potion.getColor(), ItemType.POTION);
+    }
+
     public static Potion getRandomPotion() {
         int rand = new Random().nextInt(potions.length);
-        return potions[rand];
+        return copyPotion(potions[rand]);
     }
 }
